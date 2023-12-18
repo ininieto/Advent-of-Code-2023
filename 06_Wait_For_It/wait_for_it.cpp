cc5e38a6ba9b1011140c440b91289e1c968bf704
ininieto <ininieto8@gmail.com>
2023-12-18 17:49:10 +0100

    Solved day 6

---
+++ 06_Wait_For_It/wait_for_it.cpp
@@ -0,0 +1,45 @@
+/*
+    Advent of Code 2023 - 05.12.2023
+
+    Link to the problem: https://adventofcode.com/2023/day/5
+
+    Author of the solution: Íñigo Nieto Cuadrado
+
+*/
+
+#include <iostream>
+#include <vector>
+
+int main(){
+    
+    std::vector<int> timeVec = {7, 15, 30};
+    std::vector<int> distVec = {9, 40, 200};
+    
+    int result = 1;
+    
+    for(int i = 0; i < timeVec.size(); i++){
+        
+        int currentTime = timeVec[i];
+        int currentDist = distVec[i];
+        
+        int possibleHoldTimes = 0;
+        
+        for(int holdTime = 0; holdTime <= currentTime; holdTime++){
+            
+            
+            int speed = holdTime;
+            int traveledDist = speed * (currentTime - holdTime);
+            
+            if(traveledDist > currentDist)
+                possibleHoldTimes ++;
+            else if(traveledDist < currentDist && possibleHoldTimes > 0)
+                break;
+        }
+        
+        result *= possibleHoldTimes;
+    }
+    
+    std::cout << "The result is " << result << "\n";
+
+    return 0;
+}
