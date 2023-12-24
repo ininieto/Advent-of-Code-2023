cc39960598523760cda36f041ac2c68cee3e7ea3
ininieto <ininieto8@gmail.com>
2023-12-24 16:54:46 +0100

    Created struct Pipe

--- 10_Pipe_Maze/pipe_maze.cpp
+++ 10_Pipe_Maze/pipe_maze.cpp
@@ -11,6 +11,14 @@
 #include <fstream>
 #include <vector>
 
+// Every pipe will have its position and its distance
+struct Pipe{
+
+    std::pair<int, int> position;   // not sure if I need this. Probably not    
+    char tile;  // Will contain the character (F, 7, |, ...)
+    int distance;
+};
+
 // Function to split a std::string by a specific delimitator
 std::vector<std::string> split(std::string text, std::string delim){
 
@@ -48,6 +56,38 @@ std::string readInputText(std::string inputText){
     return inputData;
 }
 
+// Function to fill in the grid
+void fillGrid(std::vector<std::vector<Pipe>> &grid, std::string input, std::pair<int, int> &startingPosition){ // 2D vector with all the grid
+
+    int nrows = grid.size(), ncols = grid[0].size();
+
+    int strCounter = 0;
+
+    for (int i = 0; i < nrows; i++){
+        for (int j = 0; j < ncols; j++){
+
+            if (input[strCounter] == '\n')
+                strCounter ++;
+            if(input[strCounter] == 'S')
+                startingPosition = std::make_pair(i, j);
+
+            grid[i][j].tile = input[strCounter];
+            grid[i][j].distance = INT_MAX;
+            grid[i][j].position = std::make_pair(i, j); // not sure if I need this. Probably not
+            strCounter ++;
+        }
+    }
+}
+
+// The big boy. I want this to be a recursive function that obtains the distance for every position
+void pipeMaze(std::pair<int, int> startingPosition, std::vector<std::vector<Pipe>> grid){
+
+
+    // Call an extern function to get boundaries
+    // For each boundary, calculate the distance (check if it's already calculated)
+    // If there are no possible jumps, end the process
+}
+
 
 int main(){
 
@@ -58,6 +98,7 @@ int main(){
         - Then comes the algorithm
         - Scan all the adjacent characters (luckily no diagonal movement)
         - Define a function isPossibleConnection() to see if we can jump to a specific position
+        - Not sure if, first of all, I should find the loop and discard the rest
         - Still unsure about how to determine the distance. Maybe I could create a class Pipe with its 
           inherited subclasses (all the possible positions of the pipe) and with a member variable "distance"
           I find it a bit too over-engineering but could be a way to refresh subclasses    
@@ -69,25 +110,16 @@ int main(){
 
     // TODO: Guess the values automatically
     int nrows = 5, ncols = 5;
-    std::vector<std::vector<char>> grid(nrows, std::vector<char>(ncols)); // 2D vector with all the grid
-    std::pair<int, int> startingPosition;
-
-
+    std::vector<std::vector<Pipe>> grid(nrows, std::vector<Pipe>(ncols)); // 2D vector with all the grid
+    
     // Save the input in a grid
-    int strCounter = 0;
+    std::pair<int, int> startingPosition;   // Will contain the position of S, the point from which we start counting the distance
+    fillGrid(grid, input, startingPosition);
 
-    for (int i = 0; i < nrows; i++){
-        for (int j = 0; j < ncols; j++){
+    // I guess the algorithm comes here
+    pipeMaze(startingPosition, grid);
 
-            if (input[strCounter] == '\n')
-                strCounter ++;
-            if(input[strCounter] == 'S')
-                startingPosition = std::make_pair(i, j);
 
-            grid[i][j] = input[strCounter];
-            strCounter ++;
-        }
-    }
 
     std::cout << "a";
 
