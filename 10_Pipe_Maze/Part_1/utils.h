a42c9e268e1ef833aff2ed45ab5db3f3c1edc604
ininieto <ininieto8@gmail.com>
2023-12-28 13:36:44 +0100

    Separated part 1 from part 2

---
+++ 10_Pipe_Maze/Part_1/utils.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream>
+#include <vector>
+
+#include "Pipe.h"
+
+std::vector<std::string> split(std::string text, std::string delim);
+std::string readInputText(std::string inputText);
+void getGridDimensions(std::string input, int &nrows, int &ncols);
+std::vector<std::pair<int, int>> getSurroundings(Pipe currentPipe, int nrows, int ncols);
+bool possibleJump(Pipe currentPipe, Pipe nextPipe);
+void addNextJump(std::vector<Pipe> &nextJumps, Pipe nextJump);
+void printDistancesGrid(std::vector<std::vector<Pipe>> grid);
+
+#endif
---
+++ 10_Pipe_Maze/Part_2/Pipe.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#ifndef PIPE 
+#define PIPE
+
+#include <iostream>
+
+class Pipe{
+
+private:
+    char tile;
+    std::pair<int, int> position;
+    int distance;
+
+public:
+
+    // Default constructor
+    Pipe();
+    // Constructor
+    Pipe(char tile, std::pair<int, int> position, int distance);
+
+    // Destructor
+    virtual ~Pipe(){};
+
+    // Getters and setters
+    void setTile(char tile);
+    void setDistance(int distance);
+    void setPosition(std::pair<int, int> position);
+    int getDistance();
+    std::pair<int, int> getPosition();
+    char getTile();
+};
+
+#endif
