381b94dfbf0ffee406e3289551a851d312ff802f
ininieto <ininieto8@gmail.com>
2024-01-07 07:01:39 +0100

    Created day 11

---
+++ 11_Cosmic_Expansion/cosmic_expansion.cpp
@@ -0,0 +1,58 @@
+/*
+    Advent of Code 2023 - 10.12.2023
+
+    Link to the problem: https://adventofcode.com/2023/day/10
+
+    Author of the solution: Íñigo Nieto Cuadrado
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+// Function to split a std::string by a specific delimitator
+std::vector<std::string> split(std::string text, std::string delim){
+
+    std::vector<std::string> splittedText;
+    size_t pos = 0;
+    std::string token;
+
+    while((pos = text.find(delim)) != std::string::npos){
+        token = text.substr(0, pos);
+        splittedText.push_back(token);
+        text.erase(0, pos + delim.length());
+    }
+
+    if(text.length() > 0)
+        splittedText.push_back(text);
+
+    return splittedText;
+}
+
+// Read input data from txt file
+std::string readInputText(std::string inputText){
+
+    std::fstream inputfile;
+    std::string inputData;
+
+    inputfile.open(inputText, std::ios::in);
+    if (inputfile.is_open()){
+        std::string tp;
+
+        while (getline(inputfile, tp)){
+            inputData += tp;
+            inputData += "\n";
+        }
+    }
+    return inputData;
+}
+
+int main(){
+
+    std::cout << "Hallo";
+
+
+
+    return 0;
+}
