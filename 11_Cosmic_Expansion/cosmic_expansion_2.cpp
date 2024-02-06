b05da2062af2b3f3238d084a8e99b4812acb8ab9
ininieto <ininieto8@gmail.com>
2024-02-06 12:03:18 +0100

    Set variables to uint64_t and created variable expansionRate

--- 11_Cosmic_Expansion/cosmic_expansion_2.cpp
+++ 11_Cosmic_Expansion/cosmic_expansion_2.cpp
@@ -90,7 +90,7 @@ void fillGrid(std::vector<std::vector<char>> &grid, std::string input){
 }
 
 // Function that expands the space
-void expandSpace(std::vector<std::vector<char>> &grid, std::vector<int> &rowsExpansionIndexes, std::vector<int> &colsExpansionIndexes){
+void expandSpace(std::vector<std::vector<char>> &grid, std::vector<int> &rowsExpansionIndexes, std::vector<int> &colsExpansionIndexes, int expansionRate){
 
     // Expand rows
     for(int i = 0; i < grid.size(); i++){
@@ -109,7 +109,7 @@ void expandSpace(std::vector<std::vector<char>> &grid, std::vector<int> &rowsExp
         if(isRowEmpty){ 
             //grid.insert(grid.begin() + i, row); // If we are here is precisely because our row was empty :)
             rowsExpansionIndexes.push_back(i);
-            i++; // Increase the iteration index
+            i += expansionRate; // Increase the iteration index
         }        
     }
 
@@ -135,16 +135,16 @@ void expandSpace(std::vector<std::vector<char>> &grid, std::vector<int> &rowsExp
                 grid[j].insert(grid[j].begin() + i, '.');
             }*/
             colsExpansionIndexes.push_back(i);
-            i++; // Increase the iteration index
+            i += expansionRate; // Increase the iteration index
         }
     }
 }
 
 
 // Function to find all the galaxies in the grid and save the coordinates
-std::vector<std::pair<int, int>> getGalaxiesCoords(std::vector<std::vector<char>> grid){
+std::vector<std::pair<uint64_t, uint64_t>> getGalaxiesCoords(std::vector<std::vector<char>> grid){
 
-    std::vector<std::pair<int, int>> galaxiesCoords;
+    std::vector<std::pair<uint64_t, uint64_t>> galaxiesCoords;
 
     for(int i = 0; i < grid.size(); i++){
         for(int j = 0; j < grid[i].size(); j++){
@@ -157,35 +157,34 @@ std::vector<std::pair<int, int>> getGalaxiesCoords(std::vector<std::vector<char>
 }
 
 // Function to apply the increase of expansion
-void correctExpansion(std::vector<std::pair<int, int>> &galaxiesCoords, std::vector<int> &rowsExpansionIndexes, std::vector<int> &colsExpansionIndexes){
+void correctExpansion(std::vector<std::pair<uint64_t, uint64_t>> &galaxiesCoords, std::vector<int> rowsExpansionIndexes, std::vector<int> colsExpansionIndexes, int expansionRate){
 
-    int expansionCorrection = 1000000 - 1;
-
-    for(auto &coord: galaxiesCoords){
+    for(std::pair<uint64_t, uint64_t> &coord: galaxiesCoords){
 
         int numCorrections = 0;
 
         // Correction on rows
-        for(int rowIndex: rowsExpansionIndexes){
-
-            if(coord.first > rowIndex)  numCorrections ++;
-        }
-        coord.first += numCorrections * expansionCorrection;
+        for(int rowIndex: rowsExpansionIndexes)
+            if(coord.first > rowIndex)  
+                numCorrections ++;
+            else    break;
+        
+        coord.first += numCorrections * expansionRate;
 
         numCorrections = 0; // Reset the number of corrections
 
         // Correction on columns
-        for(int colIndex: colsExpansionIndexes){
-
+        for(int colIndex: colsExpansionIndexes)
             if(coord.second > colIndex) numCorrections ++;
-        }
-        coord.second +=  numCorrections * expansionCorrection;
+            else break;
+
+        coord.second +=  numCorrections * expansionRate;
     }
 }
 
 
 // Function to compute the Manhattan distance
-int manhattanDist(std::pair<int, int> coords1, std::pair<int, int> coords2){
+int manhattanDist(std::pair<uint64_t, uint64_t> coords1, std::pair<uint64_t, uint64_t> coords2){
 
     return abs(coords2.first - coords1.first) + abs(coords2.second - coords1.second);
 }
@@ -205,19 +204,20 @@ int main(){
     fillGrid(grid, input);
 
     // Perform the space expansion
+    int expansionRate = 1;
     std::vector<int> rowsExpansionIndexes;  // Array that contains the indexes of the rows that must be expanded
     std::vector<int> colsExpansionIndexes;  // Array that contains the indexes of the cols that must be expanded
-    expandSpace(grid, rowsExpansionIndexes, colsExpansionIndexes);
+    expandSpace(grid, rowsExpansionIndexes, colsExpansionIndexes, expansionRate);
 
     // As we can only jump up, down, left and right, we are dealing with MANHATTAN DISTANCES
     // The general formula to compute this distance is |x2 - x1| + |y2 - y1|
 
     // Vector that contains the coordinates of all the galaxies
-    std::vector<std::pair<int, int>> galaxiesCoords = getGalaxiesCoords(grid);
-    int result = 0;
+    std::vector<std::pair<uint64_t, uint64_t>> galaxiesCoords = getGalaxiesCoords(grid);
+    uint64_t result = 0;
 
-    // Apply corrections on the coordinates --> Longer expansio
-    correctExpansion(galaxiesCoords, rowsExpansionIndexes, colsExpansionIndexes);
+    // Apply corrections on the coordinates --> Longer expansion
+    correctExpansion(galaxiesCoords, rowsExpansionIndexes, colsExpansionIndexes, expansionRate);
 
     // Calculate all the distances
     for(int i = 0; i < galaxiesCoords.size(); i++){
@@ -225,12 +225,12 @@ int main(){
             result += manhattanDist(galaxiesCoords[i], galaxiesCoords[j]);
     }
 
-    // Debug
-    //printGrid(grid);
 
     // I got an answer of 1986903970 but it is too low :( all the tests were OK
+    // Set variables to uint64_t and got 611872260002 but still too low :(
+    // When trying to replicate the first part I am getting 9483730 instead of 9556896
 
-    std::cout << result << '\n';
+    std::cout << static_cast<uint64_t>(result) << '\n';
 
     return 0;
 }
