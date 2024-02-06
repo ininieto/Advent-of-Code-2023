0b92ab6440ea2025ff0cdc7a76be48398becf511
ininieto <ininieto8@gmail.com>
2024-02-06 16:48:01 +0100

    Created day 12

---
+++ 12_Hot_Springs/hot_springs.cpp
@@ -0,0 +1,68 @@
+/*
+    Advent of Code 2023 - 12.12.2023
+
+    Link to the problem: https://adventofcode.com/2023/day/12
+
+    Author of the solution: Íñigo Nieto Cuadrado
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+// Function to split a std::string by a specific delimitator
+std::vector<std::string> split(std::string text, std::string delim){
+
+    std::vector<std::string> splittedText;
+    size_t pos = 0;
+    std::string token;
+
+    while((pos = text.find(delim)) != std::string::npos){
+        token = text.substr(0, pos);
+        splittedText.push_back(token);
+        text.erase(0, pos + delim.length());
+    }
+
+    if(text.length() > 0)
+        splittedText.push_back(text);
+
+    return splittedText;
+}
+
+// Read input data from txt file
+std::string readInputText(std::string inputText){
+
+    std::fstream inputfile;
+    std::string inputData;
+
+    inputfile.open(inputText, std::ios::in);
+    if (inputfile.is_open()){
+        std::string tp;
+
+        while (getline(inputfile, tp)){
+            inputData += tp;
+            inputData += "\n";
+        }
+    }
+    return inputData;
+}
+
+int main(){
+
+    // Store the example and the input in variables
+    std::string example = "???.### 1,1,3\n.??..??...?##. 1,1,3\n?#?#?#?#?#?#?#? 1,3,1,6\n????.#...#... 4,1,1\n????.######..#####. 1,6,5\n?###???????? 3,2,1\n";
+    std::string input = readInputText("input.txt");
+    //input = example;
+
+    // Split the initial input by lines
+    std::vector<std::string> splittedInput = split(input, "\n");
+
+
+
+
+
+
+
+    return 0;
+}
