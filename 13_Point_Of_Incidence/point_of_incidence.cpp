dcb67634490a0faddf29d082b1c05899964f0219
ininieto <ininieto8@gmail.com>
2024-02-14 12:07:34 +0100

    I think I solved part 1

--- 13_Point_Of_Incidence/point_of_incidence.cpp
+++ 13_Point_Of_Incidence/point_of_incidence.cpp
@@ -60,8 +60,8 @@ void getGridDimensions(std::string input, int &nrows, int &ncols){
     nrows ++;   // We must apply this correction
 }
 
-// Function to fill in the grid
-void fillGrid(std::vector<std::vector<char>> &grid, std::string input){
+// Function to fill in the rows
+void fillRows(std::vector<std::vector<char>> &grid, std::string input){
 
     int nrows = grid.size(), ncols = grid[0].size();
 
@@ -79,10 +79,29 @@ void fillGrid(std::vector<std::vector<char>> &grid, std::string input){
     }
 }
 
+// Function to fill in the columns
+void fillCols(std::vector<std::vector<char>> &grid, std::string input){
+
+    int nrows = grid[0].size(), ncols = grid.size(); // Transpose it for the columns
+
+    int strCounter = 0;
+
+    for (int i = 0; i < nrows; i++){
+        for (int j = 0; j < ncols; j++){
+
+            if (input[strCounter] == '\n')
+                strCounter ++;
+
+            grid[j][i] = input[strCounter];
+            strCounter ++;
+        }
+    }
+}
+
 // Recursive function to check a possible reflection
 bool checkPossibleReflection(std::vector<std::vector<char>> grid, int currentIndex, int nextIndex, bool possibleReflection){
 
-    // Base case: we ran out of rows
+    // Base case: we ran out of rows/columns
     if (currentIndex < 0 || currentIndex >= grid.size() || nextIndex < 0 || nextIndex >= grid.size())
         return possibleReflection;
 
@@ -107,7 +126,7 @@ bool checkPossibleReflection(std::vector<std::vector<char>> grid, int currentInd
 }
 
 // Function that reads a block and checks if two lines are equal. If they are, calls a recursive function to check if we have a reflection
-int getUpperRows(std::vector<std::vector<char>> grid){
+int getReflectedRowsCols(std::vector<std::vector<char>> grid){
 
     for(int i = 0; i < grid.size() - 1; i++){
 
@@ -116,28 +135,24 @@ int getUpperRows(std::vector<std::vector<char>> grid){
             return i + 1;
     }
     
-    // If we reach here means we don't have horizontal reflexes
+    // If we reach here means we don't have reflexes
     return 0;
 }
 
 int main(){
-
-    /*
-    Brief approach that will be followed:
-        - Read the input and automatically save the blocks in 2d vectors. The number of rows and cols must be guessed automatically. I might create a vector of 2d vectors, storing like that all the blocks
-        - Once I can manipulate the blocks, the first naive approach is to take all the rows/cols and start comparing them with the adjacent ones. If we find a coincidence, then jump back to the previous/next row/col to check if there is a mirror --> Might implement a recursive function
-        - That should be it :) Then finding the number of upper rows and left cols should be trivial. Should be lol with recursive functions nothing is trivial
-    */
 
    // Store the example and the input in variables
     std::string example = "#.##..##.\n..#.##.#.\n##......#\n##......#\n..#.##.#.\n..##..##.\n#.#.##.#.\n\n#...##..#\n#....#..#\n..##..###\n#####.##.\n#####.##.\n..##..###\n#....#..#\n\n";
     std::string input = readInputText("input.txt");
 
     // Just to work with example. Might comment out
-    input = example;
+    //input = example;
 
     // Separate every block and store it in a vector. Still string, not grid
     std::vector<std::string> blocksVector = split(input, "\n\n");
+
+    // Variable to hold the result
+    uint64_t result = 0;
 
     // Big loop
     for(auto block: blocksVector){
@@ -146,28 +161,19 @@ int main(){
         int nrows = 0, ncols = 0;
         getGridDimensions(block, nrows, ncols);
 
-        // Define the grid and fill it
-        std::vector<std::vector<char>> grid(nrows, std::vector<char>(ncols)); // 2D vector with all the grid
-        fillGrid(grid, block);
+        // Define a vector for the rows and another for the columns. Will be easier to perform the algorithm
+        std::vector<std::vector<char>> rows(nrows, std::vector<char>(ncols)); // 2D vector for the rows
+        std::vector<std::vector<char>> cols(ncols, std::vector<char>(nrows)); // 2D vector for the cols
+        fillRows(rows, block);
+        fillCols(cols, block);
 
-        // Find the mirrors :)
-        
-        /*
-            I don't know whether I should define a function that finds both vertical and horizontal reflexes, or just 
-            horizontal ones (easier to manipulate) and then repeat the algorithm with the transponed 2d vector. Must think
-            of that.
-
-            I'll start by implementing the rows one. I'll decide later
-        */
-
-       int numUpperRows = getUpperRows(grid);
-
-       std::cout << numUpperRows << '\n';
-
-
-
+        // Find the reflected rows and cols
+        result += (100 * getReflectedRowsCols(rows));
+        result += getReflectedRowsCols(cols);
     }
 
-
+    // Log the result
+    // I got a result of 26002 but I'm on a plane and I cannot prove if it's right haha
+    std::cout << result << '\n';
     return 0;
 }
