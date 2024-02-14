639255698c9d9d87806d23a738bc04172974e3c9
ininieto <ininieto8@gmail.com>
2024-02-14 07:30:20 +0100

    Implemented grid

--- 13_Point_Of_Incidence/point_of_incidence.cpp
+++ 13_Point_Of_Incidence/point_of_incidence.cpp
@@ -48,12 +48,71 @@ std::string readInputText(std::string inputText){
     return inputData;
 }
 
+// Function to get automatically the number of rows and columns
+void getGridDimensions(std::string input, int &nrows, int &ncols){
+
+    for(int i = 0; i < input.length(); i++){
+        if(input[i] == '\n' && ncols == 0)
+            ncols = i;
+        if(input[i] == '\n' && ncols > 0)
+            nrows ++;
+    }
+    nrows ++;   // We must apply this correction
+}
+
+// Function to fill in the grid
+void fillGrid(std::vector<std::vector<char>> &grid, std::string input){
+
+    int nrows = grid.size(), ncols = grid[0].size();
+
+    int strCounter = 0;
+
+    for (int i = 0; i < nrows; i++){
+        for (int j = 0; j < ncols; j++){
+
+            if (input[strCounter] == '\n')
+                strCounter ++;
+
+            grid[i][j] = input[strCounter];
+            strCounter ++;
+        }
+    }
+}
 
 int main(){
 
-    std::string example = "#.##..##.\n..#.##.#.\n##......#\n##......#\n..#.##.#.\n..##..##.\n#.#.##.#.\n\n#...##..#\n#....#..#\n..##..###\n#####.##.\n#####.##.\n..##..###\n#....#..#\n";
+    /*
+    Brief approach that will be followed:
+        - Read the input and automatically save the blocks in 2d vectors. The number of rows and cols must be guessed automatically. I might create a vector of 2d vectors, storing like that all the blocks
+        - Once I can manipulate the blocks, the first naive approach is to take all the rows/cols and start comparing them with the adjacent ones. If we find a coincidence, then jump back to the previous/next row/col to check if there is a mirror --> Might implement a recursive function
+        - That should be it :) Then finding the number of upper rows and left cols should be trivial. Should be lol with recursive functions nothing is trivial
+    */
 
-    std::cout << example;
+   // Store the example and the input in variables
+    std::string example = "#.##..##.\n..#.##.#.\n##......#\n##......#\n..#.##.#.\n..##..##.\n#.#.##.#.\n\n#...##..#\n#....#..#\n..##..###\n#####.##.\n#####.##.\n..##..###\n#....#..#\n";
+    std::string input = readInputText("input.txt");
+
+    // Just to work with example. Might comment out
+    //input = example;
+
+    // Separate every block and store it in a vector. Still string, not grid
+    std::vector<std::string> blocksVector = split(input, "\n\n");
+
+    // Big loop
+    for(auto block: blocksVector){
+
+        // Guess the size of the grid
+        int nrows = 0, ncols = 0;
+        getGridDimensions(block, nrows, ncols);
+
+        // Define the grid and fill it
+        std::vector<std::vector<char>> grid(nrows, std::vector<char>(ncols)); // 2D vector with all the grid
+        fillGrid(grid, block);
+
+
+
+    }
+
 
     return 0;
 }
