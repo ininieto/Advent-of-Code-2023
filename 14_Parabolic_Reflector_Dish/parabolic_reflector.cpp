4afe0eb9918aa162c7a5899763ad3f309acc05aa
ininieto <ininieto8@gmail.com>
2024-02-26 19:54:25 +0100

    Created grid

--- 14_Parabolic_Reflector_Dish/parabolic_reflector.cpp
+++ 14_Parabolic_Reflector_Dish/parabolic_reflector.cpp
@@ -48,11 +48,54 @@ std::string readInputText(std::string inputText){
     return inputData;
 }
 
+// Function to get automatically the number of rows and columns
+void getGridDimensions(std::string input, int &nrows, int &ncols){
+
+    for(int i = 0; i < input.length(); i++){
+        if(input[i] == '\n' && ncols == 0)
+            ncols = i;
+        if(input[i] == '\n' && ncols > 0)
+            nrows ++;
+    }
+}
+
+// Function to fill in the grid
+void fillGrid(std::vector<std::vector<char>> &grid, std::string input){
+
+    int nrows = grid.size(), ncols = grid[0].size();
+
+    int strCounter = 0;
+
+    for (int i = 0; i < nrows; i++){
+        for (int j = 0; j < ncols; j++){
+
+            if (input[strCounter] == '\n')
+                strCounter ++;
+
+            grid[i][j] = input[strCounter];
+            strCounter ++;
+        }
+    }
+}
+
 int main(){
 
     // Save the example and the input in variables
     std::string example = "O....#....\nO.OO#....#\n.....##...\nOO.#O....O\n.O.....O#.\nO.#..O.#.#\n..O..#O..O\n.......O..\n#....###..\n#OO..#....\n";
     std::string input = readInputText("input.txt");
+
+    // Debug
+    //input = example;
+
+    // Split the input and store each line in a vector
+    std::vector<std::string> splittedInput = split(input, "\n");
+
+    // Store all the information in a grid
+    int nrows = 0, ncols = 0;
+    getGridDimensions(input, nrows, ncols);
+    
+    std::vector<std::vector<char>> grid(nrows, std::vector<char>(ncols)); // 2D vector for the rows
+    fillGrid(grid, input);
 
 
     return 0;
