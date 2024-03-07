a45ff40328f8a41fbce0285f191eb3388371bb12
ininieto <ininieto8@gmail.com>
2024-03-07 15:47:16 +0100

    Created file for day 15

---
+++ 15_Lens_Library/lens_library.cpp
@@ -0,0 +1,64 @@
+/*
+    Advent of Code 2023 - 15.12.2023
+
+    Link to the problem: https://adventofcode.com/2023/day/15
+    
+    Author of the solution: Íñigo Nieto Cuadrado
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+// Function to split a std::string by a specific delimitator
+std::vector<std::string> split(std::string text, std::string delim){
+
+    std::vector<std::string> splittedText;
+    size_t pos = 0;
+    std::string token;
+
+    while((pos = text.find(delim)) != std::string::npos){
+        token = text.substr(0, pos);
+        splittedText.push_back(token);
+        text.erase(0, pos + delim.length());
+    }
+
+    if(text.length() > 0)
+        splittedText.push_back(text);
+
+    return splittedText;
+}
+
+// Read input data from txt file
+std::string readInputText(std::string inputText){
+
+    std::fstream inputfile;
+    std::string inputData;
+
+    inputfile.open(inputText, std::ios::in);
+    if (inputfile.is_open()){
+        std::string tp;
+
+        while (getline(inputfile, tp)){
+            inputData += tp;
+            inputData += "\n";
+        }
+    }
+    return inputData;
+}
+
+int main(){
+
+    // Save the example and the input in variables
+    std::string example = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+    std::string input = readInputText("input.txt");
+
+    // Debug
+    //input = example;
+
+    std::cout << input << '\n';
+
+
+    return 0;
+}
