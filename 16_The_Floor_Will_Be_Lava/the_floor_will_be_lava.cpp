798b52952215c571aab1a378805a4bc92931b39d
ininieto <ininieto8@gmail.com>
2024-03-09 17:28:17 +0100

    Defined grid of Nodes

--- 16_The_Floor_Will_Be_Lava/the_floor_will_be_lava.cpp
+++ 16_The_Floor_Will_Be_Lava/the_floor_will_be_lava.cpp
@@ -11,6 +11,20 @@
 #include <fstream>
 #include <vector>
 
+struct Node{
+
+    int x, y;
+    char tile;
+    bool energized = false;
+
+    // Default constructor
+    Node(): x(-1), y(-1), tile('?') {}
+
+    // Constructor with parameters
+    Node(int x_p, int y_p, char tile_p): x(x_p), y(y_p), tile(tile_p) {}
+
+};
+
 // Function to split a std::string by a specific delimitator
 std::vector<std::string> split(std::string text, std::string delim){
 
@@ -48,11 +62,52 @@ std::string readInputText(std::string inputText){
     return inputData;
 }
 
+// Function to get automatically the number of rows and columns
+void getGridDimensions(std::string input, int &nrows, int &ncols){
+
+    for(int i = 0; i < input.length(); i++){
+        if(input[i] == '\n' && ncols == 0)
+            ncols = i;
+        if(input[i] == '\n' && ncols > 0)
+            nrows ++;
+    }
+}
+
+// Function to fill in the grid
+void fillGrid(std::vector<std::vector<Node>> &grid, std::string input){
+
+    int nrows = grid.size(), ncols = grid[0].size();
+    int strCounter = 0;
+
+    for (int i = 0; i < nrows; i++){
+        for (int j = 0; j < ncols; j++){
+
+            if (input[strCounter] == '\n')
+                strCounter ++;
+
+            grid[i][j].x = i;
+            grid[i][j].y = j;
+            grid[i][j].tile = input[strCounter];
+            strCounter ++;
+        }
+    }
+}
+
 int main(){
 
     // Read the input
     std::string example = readInputText("example.txt");
     std::string input = readInputText("input.txt");
+
+    // Debug
+    input = example;
+
+    // I want to have a 2d grid of Nodes
+    int nrows = 0, ncols = 0;
+    getGridDimensions(input, nrows, ncols);
+
+    std::vector<std::vector<Node>> grid(nrows, std::vector<Node>(ncols)); // 2D vector for the rows
+    fillGrid(grid, input);
 
     std::cout << example << '\n';
 
